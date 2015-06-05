5de72cf7f3b497ec19023be1ec149640c8c3b3d4
Guilherme Gadelha <guilhermemgadelha@gmail.com>
2015-06-05 18:15:28 -0500

    intial commit

---
+++ execExamples.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Script to execute a serie of examples over the different hadoop
+# schedulers. It generates a final csv with KPI's indicating the
+# different key performance indicators of each different scheduler.
+
+
